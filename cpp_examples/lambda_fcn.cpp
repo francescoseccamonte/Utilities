cfd2baf4640bd9058ee7db264bdf1341c74cf6b9
Francesco Seccamonte <francesco@nutonomy.com>
2018-02-01 21:26:35 +0800

    Added lambda functions + std::transform example

---
+++ cpp_examples/lambda_fcn.cpp
@@ -0,0 +1,57 @@
+// Test lambda functions and std::transform
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+	std::size_t size = 5;
+
+	std::vector<int> vec(size);
+
+	int a = 2;
+
+	
+	std::transform(	vec.begin(),
+					vec.end(),
+					vec.begin(),
+					[](int x){return x+1;});
+	
+
+	std::transform(	vec.begin(),
+					vec.end(),
+					vec.begin(),
+					[&](auto x){++a; return x+a;});	// capturing by value gives a compile time error
+
+	std::transform(	vec.begin(),
+					vec.end(),
+					vec.begin(),
+					[=](int x) -> int {return x+a;});	// -> specifies the return type
+
+	std::vector<int> vec1(size);
+
+	std::transform(	vec.begin(),
+					vec.end(),
+					vec1.begin(),	// WARNING : need to check first that vec1 can contain all the elements
+					[=](int x) -> int {return x+a;});
+
+	std::vector<int> vec2(size);
+
+	std::transform(	vec.begin(),
+					vec.end(),
+					vec1.begin(),	// iterating also on vec1 (that can be bigger than vec)
+					vec2.begin(),
+					[](int x, int y){return x+y;});
+
+	for (auto& i : vec)
+		std::cout << i << "\n";
+
+	for (auto& i : vec1)
+		std::cout << i << "\n";
+
+	for (auto& i : vec2)
+		std::cout << i << "\n";
+
+	return 0;
+}
